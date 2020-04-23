ff88441e7154ad7220d693c963b93fb03368d39c
doriclaudino <dori.claudino@gmail.com>
2020-04-22 23:05:22 -0400

    improv configs for debug

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
   verbose: true,
   collectCoverage: true,
   collectCoverageFrom: ["src/*.js"],
+  testURL: "https://www.somthing.com/test.html",
   coverageDirectory: "coverage",
   testEnvironment: "jest-environment-jsdom-sixteen",
   setupFiles: ["<rootDir>/scripts/setupFile.js"],
