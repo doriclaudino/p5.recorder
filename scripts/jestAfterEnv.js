df09358ae496787b9103572965482fc04f9c1c63
doriclaudino <dori.claudino@gmail.com>
2020-04-22 23:06:24 -0400

    fix typo

--- scripts/jestAfterEnv.js
+++ scripts/jestAfterEnv.js
@@ -3,8 +3,8 @@
  * -  create container and instance before every test
  * -  delete container and instance after every test
  */
-global.myp5;
-global.myp5Container;
+global.myp5 = undefined;
+global.myp5Container = undefined;
 
 beforeEach(done => {
   global.myp5Container = document.createElement("div");
@@ -15,12 +15,13 @@ beforeEach(done => {
     p.setup = function() {
       p.createCanvas(200, 200);
       global.myp5 = p;
+      myp5.canvas.captureStream = function() {};
       done();
     };
     p.draw = () => {
       p.background(0);
       p.fill(255);
-      p.rect(sketch.random(x), p.random(y), 50, 50);
+      p.rect(p.random(x), p.random(y), 50, 50);
     };
   }, global.myp5Container);
 });
